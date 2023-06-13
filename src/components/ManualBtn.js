1643e3ccd6577668798ce6ac8dd5da99b76a54f4
musthopa <dxdskipper@gmail.com>
2023-06-13 22:42:46 +0700

    fixed wa url

--- src/components/ManualBtn.js
+++ src/components/ManualBtn.js
@@ -15,7 +15,7 @@ export default function ManualBtn({ order }) {
     <>
       <Link
         target="_blank"
-        href={`https://wa.me/082236900924?text=Saya%20ingin%20konfirmasi%20pembayaran%0ANo%20Pesanan%20:%20${order._id}%0ANama%20:%20${order.user.name}%0Aemail%20:%20${order.user.email}%0Ano%20telepon%20:%20${order.handphone}%0Aproduk%20:%20sarung%0Anominal%20:%20${rupiahFormat}`}
+        href={`https://wa.me/+6282236900924?text=Saya%20ingin%20konfirmasi%20pembayaran%0ANo%20Pesanan%20:%20${order._id}%0ANama%20:%20${order.user.name}%0Aemail%20:%20${order.user.email}%0Ano%20telepon%20:%20${order.handphone}%20Nominal%20:%20${rupiahFormat}`}
       >
         <button className="mb-4">Bayar</button>
       </Link>
